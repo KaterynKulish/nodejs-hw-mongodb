0666c578db609bd1c5757fd2c427883e6688cadb
Kateryna Kulish <kulishk26@gmail.com>
2025-05-07 18:19:33 +0300

    HW-6 fix name

--- src/db/models/Contact.js
+++ src/db/models/Contact.js
@@ -24,7 +24,7 @@ const contactSchema = new Schema(
       default: 'personal',
       required: true,
     },
-    posterUrl: {
+    photo: {
       type: String,
       required: false,
       default: null,
